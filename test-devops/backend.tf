a9c175b7ed202187dd16b88aaa75f465e9668261
Bruno <bruno_lassakoski@hotmail.com>
2025-03-11 20:49:15 -0300

    alteração tf

--- test-devops/backend.tf
+++ test-devops/backend.tf
@@ -2,14 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "random_string" "suffix" {
-  length  = 6
-  special = false
-  upper   = false
-}
-
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "meu-terraform-state-bucket-${random_string.suffix.result}"
+  bucket = "meu-terraform-bucket-BL"
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
@@ -27,9 +21,81 @@ resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
     status = "Enabled"
 
     noncurrent_version_expiration {
-      noncurrent_days = 30  # Exclui versões antigas depois de 30 dias
+      noncurrent_days = 30  # Exclui versões antigas após 30 dias
     }
   }
+}
+
+resource "aws_iam_user" "terraform_user" {
+  name = "terraform-backend-user"
+}
+
+resource "aws_iam_access_key" "terraform_key" {
+  user = aws_iam_user.terraform_user.name
+}
+
+resource "aws_iam_policy" "terraform_s3_policy" {
+  name        = "TerraformS3Policy"
+  description = "Permite que o Terraform acesse o S3 para armazenar o state"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::${aws_s3_bucket.terraform_state.id}",
+          "arn:aws:s3:::${aws_s3_bucket.terraform_state.id}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "terraform_user_policy_attachment" {
+  user       = aws_iam_user.terraform_user.name
+  policy_arn = aws_iam_policy.terraform_s3_policy.arn
+}
+
+output "aws_access_key_id" {
+  value     = aws_iam_access_key.terraform_key.id
+  sensitive = true
+}
+
+output "aws_secret_access_key" {
+  value     = aws_iam_access_key.terraform_key.secret
+  sensitive = true
+}
+
+terraform {
+  backend "s3" {
+    bucket        = "meu-terraform-bucket-BL"
+    key           = "terraform/state.tfstate"
+    region        = "us-east-1"
+    encrypt       = true
+    use_lockfile  = true
+  }
+}
+
+resource "tls_private_key" "my_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "minha-chave-ssh"
+  public_key = tls_private_key.my_key.public_key_openssh
+}
+
+output "private_key_pem" {
+  value     = tls_private_key.my_key.private_key_pem
+  sensitive = true
 }
 
 resource "aws_instance" "k8s_ec2" {
@@ -69,7 +135,7 @@ resource "aws_instance" "k8s_ec2" {
 
 resource "aws_security_group" "k8s_sg" {
   name        = "k8s-security-group"
-  description = "libera as portas ssg e do k8s"
+  description = "libera as portas SSH e do K8s"
 
   ingress {
     from_port   = 22
@@ -86,16 +152,9 @@ resource "aws_security_group" "k8s_sg" {
   }
 
   egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }  
 }
